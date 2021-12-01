01add74fa147c2eb2fd1329b57b6ea124a9201cd
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2021-12-01 15:10:58 -0600

    added dbt-scaffold to core/scripts to testing adding new scaffolding repo to core

---
+++ core/scripts/adapter-scaffold/hooks/pre_gen_project.py
@@ -0,0 +1,7 @@
+{{ cookiecutter.update({ "adapter_upper":cookiecutter.directory_name.upper() }) }}
+
+{% if cookiecutter.adapter_src == "sql" %}
+{{ cookiecutter.update({ "adapter_cls":"SQLAdapter","connection_cls": "SQLConnectionManager"}) }}
+{% else %}
+{{ cookiecutter.update({"adapter_cls":"BaseAdapter","connection_cls":"BaseConnectionManager"}) }}
+{% endif %}
