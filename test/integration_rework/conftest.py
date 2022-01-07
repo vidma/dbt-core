118ad8412949ba1c28678e6396ab437a54e217ac
Emily Rockman <emily.rockman@dbtlabs.com>
2022-01-07 16:05:28 -0600

    separate new and old folders

--- test/integration_rework/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def pytest_collection_modifyitems(items):
-    for item in items:
-        if "interface" in item.nodeid:
-            item.add_marker(pytest.mark.interface)
-        elif "event" in item.nodeid:
-            item.add_marker(pytest.mark.event)
